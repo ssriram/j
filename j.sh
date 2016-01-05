b60ae2161561c273c00827bd550e2475cc595c4a
ssriram <naughtysriram@gmail.com>
2016-01-06 00:16:49 +0530

    added functionality to add arbitrary paths and checks

--- j.sh
+++ j.sh
@@ -18,6 +18,7 @@ function j(){
     -h|--help)
         echo "j -h|--help               show help"
         echo "j -a <name>               add new entry for pwd with key <name>"
+        echo "j -a <name> <dir>         add new entry for <dir> with key <name>"
         echo "j -d <name>               delete entry for key <name>"
         echo "j -l                      list all entries"
         echo "j -r                      reload all entries"
@@ -27,12 +28,23 @@ function j(){
 
     # add directory entry
     -a)
-        J[$2]=`pwd`
+        if [ -n "$3" ]; then
+            J[$2]="$3"
+        elif [ -n "$2" ]; then
+            J[$2]=`pwd`
+        else
+            echo "j -a <name>               add new entry for pwd with key <name>"
+            echo "j -a <name> <dir>         add new entry for <dir> with key <name>"
+        fi
         ;;
 
     # delete directory entry
     -d)
-        unset J[$2]
+        if [ -n "$2" ]; then
+            unset J[$2]
+        else
+            echo "j -d <name>               delete entry for key <name>"
+        fi
         ;;
 
     # list all entries
@@ -51,7 +63,9 @@ function j(){
 
     # persist changes
     -s)
-        echo "" > $JFILE
+        if [ -f $JFILE ]; then
+            rm $JFILE
+        fi
         for jk in "${!J[@]}"; do
           echo "J[\"${jk}\"]=\"${J[$jk]}\"" >> $JFILE
         done
@@ -59,7 +73,11 @@ function j(){
 
     # by default jump
     *)
-        cd "${J[$1]}"
+        if [ ${J["$1"]+yes} ]; then
+            cd "${J[$1]}"
+        else
+            echo "J[\"$1\"] entry not found."
+        fi
         ;;
   esac
 }
