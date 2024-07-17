5af9aed0edb237f6cbbcfae82872e891839c8b49
ssriram <pingssriram@gmail.com>
2024-07-17 23:42:17 +0530

    add default jump with cd; 'j -' to toggle jumps; add license to code

--- j.sh
+++ j.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/bash
 
+#
+# 
+# Copyright (c) 2016-2024 Sriram Srinivasan.
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to
+# whom the Software is furnished to do so, subject to the
+# following conditions:
+# 
+# The above copyright notice and this permission notice shall
+# be included in all copies or substantial portions of the
+# Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+# 
+
 # A small tool to quickly jump between directories
 
 JFILE=~/.jfile
@@ -10,7 +38,36 @@ if [ -f $JFILE ]; then
     source $JFILE
 fi
 
-function j(){
+
+function _j_jump() {
+		if [ "$1" == "~" ]; then
+				J["~"]=~
+		fi
+		if [ "${J["-"]}" == "" ]; then
+				J["-"]=`pwd`
+		fi
+    if [ ${J["$1"]+yes} ]; then
+				_d=${J["$1"]}
+        if [ -d "$_d" ]; then
+						if [ "$_d" == "`pwd`" ]; then
+								:
+						else
+								J["-"]=`pwd`
+								cd "$_d"
+						fi
+        fi
+    else
+		    # J entry not found; so try cd
+        if [ -d "$1" ]; then
+            cd "$1"
+        else
+						#TODO: may be add some fuzzy search
+            echo "J[\"$1\"] entry or dir: $1 not found"
+        fi
+    fi
+}
+
+function j() {
 
   case "$1" in
 
@@ -71,13 +128,13 @@ function j(){
         done
         ;;
 
-    # by default jump
+    # by default just jump
     *)
-        if [ ${J["$1"]+yes} ]; then
-            cd "${J[$1]}"
-        else
-            echo "J[\"$1\"] entry not found."
-        fi
+				if [ "$1" == "" ]; then
+						_j_jump "-"
+				else
+						_j_jump "$@"
+				fi
         ;;
   esac
 }
